d369f4adca79a34a4c91ffd65af16e5522f2d27d
Kirti Gopal Saraswat <118678673+kirtigopalcric@users.noreply.github.com>
2024-11-04 16:11:47 +0530

    Add files via upload

---
+++ Daily-Tasks/Daily-task-8/two.js
@@ -0,0 +1,64 @@
+// Primitive Data type
+
+//String
+const name = "Karan Sharma"
+console.log(`Name :- ${name}`);
+
+//type of
+
+console.log(typeof(name));
+
+
+//Number
+const age = 22
+console.log(`Age :- ${age}`);
+
+//type of
+
+console.log(typeof(age));
+
+
+//Boolean
+const isAvailable = true;
+console.log(`Boolean ;- ${isAvailable}`);
+
+//typeof
+
+console.log(typeof(isAvailable));
+
+
+
+//Undefined
+
+const x = undefined ;
+
+console.log(`Undefine :- ${x} `);
+
+//type of
+
+console.log(typeof(x));
+
+
+
+//Null
+
+const value = null;
+console.log(`Null :- ${value}`);
+
+//type of
+
+console.log(typeof(value));
+
+//Object
+
+const obj = {
+    name : "Kgopal",
+    roll_No : 24
+}
+
+console.log(obj);
+
+//TypeOf
+
+console.log(typeof(obj));
+
