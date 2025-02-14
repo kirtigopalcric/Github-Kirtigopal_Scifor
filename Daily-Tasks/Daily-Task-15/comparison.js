a7b2d21d17aa9ec7b1223e1aa6174076fbb8a8e6
Kirti Gopal Saraswat <118678673+kirtigopalcric@users.noreply.github.com>
2025-02-14 22:22:25 +0530

    Add files via upload

---
+++ Daily-Tasks/Daily-Task-15/comparison.js
@@ -0,0 +1,13 @@
+
+var a = 25;
+var b = "11";
+
+console.log("a is equal to b: ", a == b);
+console.log("a is not equal to b: ", a != b);
+console.log("a is equal to b: ", a === b);
+console.log("a is not equal to b: ", a !== b);
+console.log("a > b: ", a > b);
+console.log("a < b: ", a < b);
+console.log("a >= b: ", a >= b);
+console.log("a <= b: ", a <= b);
+console.log("a <= b: ", a <= b);
