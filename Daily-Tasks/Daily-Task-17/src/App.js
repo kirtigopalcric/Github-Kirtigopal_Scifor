07a54ab5dcd320912b807ececbbfee70d1e958c4
Kirti Gopal Saraswat <118678673+kirtigopalcric@users.noreply.github.com>
2025-02-15 17:19:24 +0530

    Add files via upload

---
+++ Daily-Tasks/Daily-Task-17/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the Heading</h1>
+      <p>This is a paragraph describing the heading above. React makes it easy to build UI components!</p>
+    </div>
+  );
+}
+
+export default App;
